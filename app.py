d29c02268aadcc7c2f042450bd020dbc4866c9d5
Coletronix <colesfirecar@gmail.com>
2024-04-04 12:16:57 -0400

    add robots to simulator

--- app.py
+++ app.py
@@ -12,23 +12,26 @@ from gui.gui import PlotGUI
 from gui.netgrid_model import NetGridPresenter
 from network.faces import Direction
 from network.network_grid import NetworkGrid
-from routing_algorithms.template import Template
+import routing_algorithms.template as routet
+import robot_algorithm.template as robt
+
 
 UNIVERSE_DIMENSIONS = (3,3,3)
 
 
 def init_simulator() -> NetworkGrid:
     # Main grid
-    grid = NetworkGrid(Template())
+    grid = NetworkGrid(routet.Template(), robt.Template())
 
     # Add some nodes to the grid
     grid.add_node(0, 0, 0)
     grid.add_node(1, 0, 0)
     grid.add_node(1, 1, 0)
     grid.add_node(2, 0, 0)
+    grid.add_robot(0, 1, 0)
     
     # Initiate a packet transmission
-    grid.get_node(2, 0, 0).send_packet(Direction.WEST, "Hello")
+    # grid.get_node(2, 0, 0).send_packet(Direction.WEST, "Hello")
 
     return grid
 
@@ -39,8 +42,6 @@ def main():
     model = NetGridPresenter(simulator, UNIVERSE_DIMENSIONS)
     ui = PlotGUI(UNIVERSE_DIMENSIONS, model)
     model.add_observer(ui)
-
-    # Display window
     plt.show()
 
 
