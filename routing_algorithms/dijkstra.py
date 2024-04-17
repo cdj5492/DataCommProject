30abab753a1225c11d566905074a32e809b8d504
Max Wozniak <maw2647@rit.edu>
2024-04-17 15:09:00 -0400

    Wrote entire dijkstra algorithm, may need to alter init file for it to work

--- routing_algorithms/dijkstra.py
+++ routing_algorithms/dijkstra.py
@@ -1,19 +1,195 @@
 import dataclasses
-import typing
+from typing import Dict, Optional
 
 from network.robot import Robot
 from network.routing_cube import RoutingCube
 from network.faces import Direction
 from robot_algorithm.robot_algorithm import RobotAlgorithm
 from routing_algorithms.helpers import node_addr_t, determine_tx_dir, determine_tx_addr
-from routing_algorithms.routing_algorithm import RoutingAlgorithm
-
-BMF_DEFAULT_LINK_COST = 1
 
 
-import heapq
-import sys
+@dataclasses.dataclass
+class DijkstraDistanceTbl:
+    """
+    Dijkstra Distance Table
+    """
 
+    def __init__(self, my_addr: node_addr_t):
+        """
+        Create a distance table for a specific node.
+
+        :param my_addr: address of the node that owns this distance table
+        """
+        self.my_addr = my_addr
+        self._distances: Dict[node_addr_t, int] = {}
+        self._visited: Dict[node_addr_t, bool] = {}
+
+    def new_neighbor(self, addr: node_addr_t, link_cost: int):
+        """
+        Update the distance table with a neighbor node (i.e., distance to addr via itself
+        is link_cost).
+
+        :param addr: neighbor node address
+        :param link_cost: distance from this node to neighbor node
+        """
+        self._distances[addr] = link_cost
+        self._visited[addr] = False
+
+    def update(self, distance_vector: Dict[node_addr_t, int], via: node_addr_t):
+        """
+        Update the distance table using a distance vector received from a neighbor node
+        (i.e., distance to destination via neighbor is distance from neighbor to
+        destination plus distance to neighbor).
+
+        :param distance_vector: mapping of destination addresses to partial link costs
+        :param via: neighbor node address (address of the node to which the distances in
+                    distance_vector are related)
+        """
+        for dest, distance in distance_vector.items():
+            if dest != self.my_addr:
+                new_distance = distance + self._distances.get(via, float("inf"))
+                if new_distance < self._distances.get(dest, float("inf")):
+                    self._distances[dest] = new_distance
+
+    def next_hop(self) -> Optional[node_addr_t]:
+        """
+        Find the unvisited neighbor with the minimum distance.
+
+        :return: address of the neighbor with the minimum distance, or None if all
+                 neighbors are visited
+        """
+        min_dist = float("inf")
+        min_neighbor = None
+        for neighbor, distance in self._distances.items():
+            if not self._visited[neighbor] and distance < min_dist:
+                min_dist = distance
+                min_neighbor = neighbor
+        return min_neighbor
+
+    def mark_visited(self, addr: node_addr_t):
+        """
+        Mark a node as visited in the Dijkstra algorithm.
+
+        :param addr: address of the node to mark as visited
+        """
+        self._visited[addr] = True
+
+class DijkstraData:
+    """
+    Encapsulates a DijkstraDistanceTbl with other state relevant for Dijkstra routing.
+
+    This class should be used for RoutingCube.data.
+    """
+
+    def __init__(self, my_addr: node_addr_t):
+        """
+        Create new Dijkstra data with an empty distance table.
+
+        :param my_addr: address of the node that owns this data
+        """
+        self.distance_tbl = DijkstraDistanceTbl(my_addr)
+
+    def new_neighbor(self, addr: node_addr_t, link_cost: int):
+        """
+        Wrapper for DijkstraDistanceTbl.new_neighbor().
+        """
+        return self.distance_tbl.new_neighbor(addr, link_cost)
+
+    def update(self, distance_vector: Dict[node_addr_t, int], via: node_addr_t):
+        """
+        Wrapper for DijkstraDistanceTbl.update().
+        """
+        return self.distance_tbl.update(distance_vector, via)
+
+    def next_hop(self) -> Optional[node_addr_t]:
+        """
+        Wrapper for DijkstraDistanceTbl.next_hop().
+        """
+        return self.distance_tbl.next_hop()
+
+    def mark_visited(self, addr: node_addr_t):
+        """
+        Mark a node as visited.
+        """
+
+class DijkstraRouting(RobotAlgorithm):
+    """
+    Dijkstra routing algorithm implementation.
+    """
+
+    def __init__(self, robot: Robot = None, cube: RoutingCube = None):
+        """
+        Initialize the routing algorithm with the robot and routing cube.
+
+        :param robot: robot instance (default: None)
+        :param cube: routing cube instance (default: None)
+        """
+        super().__init__(robot, cube)  # Pass arguments to RobotAlgorithm
+        # ... (rest of the constructor logic)
+
+        if robot is None or cube is None:
+            raise ValueError("Both robot and cube are required for DijkstraRouting.")
+
+    def power_on(self, cube: RoutingCube) -> None:
+        """
+        Called when the routing algorithm is powered on.
+
+        :param cube: routing cube instance
+        """
+        # ... (Perform any Dijkstra-specific initialization on the cube)
+        pass
+
+    def step(self, robot: Robot) -> None:
+        """
+        Perform a routing step based on the Dijkstra algorithm.
+
+        This function should use the robot and routing cube to exchange routing
+        information, update the distance table, and potentially transmit data packets.
+
+        :param robot: robot instance
+        """
+        # 1. Receive packets from neighbors
+        routing_packets = self.cube.recv_packets(robot)
+        # 2. Update distance table based on received packets (using data.update())
+        for packet in routing_packets:
+            self.data.update(packet.distance_vector, packet.source)
+
+        # 3. If there's a packet to deliver, determine next hop using data.next_hop()
+        if self.has_packet_to_deliver():
+            next_hop = self.data.next_hop()
+            if next_hop:
+                # 4. Transmit packet to the next hop using robot.transmit()
+                self.robot.transmit(self.get_packet_to_deliver(), determine_tx_dir(next_hop))
+
+        # 5. Mark visited nodes (optional, might be handled implicitly)
+
+    def has_packet_to_deliver(self) -> bool:
+        """
+        Check if the robot has a packet to deliver based on its routing table.
+
+        :return: True if the robot has a packet to deliver, False otherwise
+        """
+        # Implement logic to check if the robot has a packet for a destination
+        # that can be reached based on the current distance table
+        pass
+
+class DijkstraRobot(Robot):
+    """
+    Robot class with Dijkstra routing algorithm.
+    """
+
+    def __init__(self, addr: int, routing_alg: DijkstraRouting):
+        """
+        Initialize the robot with its address and routing algorithm.
+
+        :param addr: robot address
+        :param routing_algo: routing algorithm instance
+        """
+        super().__init__(addr)
+        self.routing_alg = routing_alg
+
+
+"""
 # Class representing a node in the graph
 class Node:
     def __init__(self, name):
@@ -54,28 +230,4 @@ def get_shortest_path(target_node):
         path.insert(0, current_node.name)
         current_node = current_node.previous
     return path
-
-# Sample graph
-graph = {
-    'A': Node('A'),
-    'B': Node('B'),
-    'C': Node('C'),
-    'D': Node('D'),
-    'E': Node('E'),
-}
-
-graph['A'].add_neighbor(graph['B'], 4)
-graph['A'].add_neighbor(graph['C'], 2)
-graph['B'].add_neighbor(graph['D'], 5)
-graph['C'].add_neighbor(graph['B'], 1)
-graph['C'].add_neighbor(graph['D'], 8)
-graph['D'].add_neighbor(graph['E'], 3)
-
-# Execute Dijkstra algorithm
-start_node = graph['A']
-dijkstra(start_node)
-
-# Get shortest path to node E
-target_node = graph['E']
-shortest_path = get_shortest_path(target_node)
-print("Shortest path to node E:", shortest_path)
+"""
